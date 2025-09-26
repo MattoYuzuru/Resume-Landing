55145669b1811a924ac3f448aa8d31eca473396c
Matvei Riabushkin <matveybobr937@gmail.com>
2025-09-26 14:45:59 +0300

    fix: Форма всё еще не читается, поменял считвание

--- backend/api.py
+++ backend/api.py
@@ -1,17 +1,18 @@
-from typing import Annotated
-
-from fastapi import FastAPI, Form
-
+from fastapi import FastAPI, Request
 from util import send_mail
 
 app = FastAPI()
 
 
 @app.post("/api/send-mail")
-async def post_send_mail(
-        name: Annotated[str, Form()],
-        email: Annotated[str, Form()],
-        message: Annotated[str, Form()]
-):
+async def post_send_mail(request: Request):
+    form_data = await request.form()
+    name = form_data.get("name")
+    email = form_data.get("email")
+    message = form_data.get("message")
+
+    if not all([name, email, message]):
+        return {"error": "Missing required fields"}
+
     send_mail(name, email, message)
     return {"status": "ok"}
